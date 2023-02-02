c62f108d565fcfb27abf205576743935a4c52cc6
rafaeldpsilva <1190979@isep.ipp.pt>
2023-02-02 17:40:03 +0000

    path

--- utils/utils.py
+++ utils/utils.py
@@ -2,5 +2,5 @@ import json
 import sys
 sys.path.append('.')
 def get_config():
-    with open('../config/config.json') as config_file:
+    with open('./config/config.json') as config_file:
         return json.load(config_file)
