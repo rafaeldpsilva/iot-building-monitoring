ae85a60f7069bff478c49d3e950e5404b11a2ded
rafaeldpsilva <1190979@isep.ipp.pt>
2023-02-06 17:34:42 +0000

    monitoring on insert

--- database/TokenRepository.py
+++ database/TokenRepository.py
@@ -4,13 +4,13 @@ from utils import utils
 
 class TokenRepository:
     def __init__(self):
-        config = utils.get_config()
+        self.config = utils.get_config()
 
-        self.server = str(config['storage']['local']['server'])
-        self.port = str(config['storage']['local']['port'])
+        self.server = str(self.config['storage']['local']['server'])
+        self.port = str(self.config['storage']['local']['port'])
 
         # criar a tabela
-        self.TOKEN = config['storage']['local']['token']
+        self.TOKEN = self.config['storage']['local']['token']
         
     def client(self):
         return MongoClient(self.server + ':' + self.port)
@@ -24,12 +24,16 @@ class TokenRepository:
     def insert_token(self, token, expiration_time_minutes, datetime):
         try:
             client = self.client()
-            # inserir objeto em forma de dicionario em mongodb
-            client[self.TOKEN[0]][self.TOKEN[1]].insert_one({"token": token,
-                                    "expiration_time_minutes": expiration_time_minutes,
-                                    "datetime": datetime,
-                                    "active": True})
+            token = {"token": token,
+                        "expiration_time_minutes": expiration_time_minutes,
+                        "datetime": datetime,
+                        "active": True}
+            client[self.TOKEN[0]][self.TOKEN[1]].insert_one(token)
             client.close()
+
+            if self.config['app']['monitoring']:
+                print('\nToken\n',token)
+
         except ConnectionError as exc:
             raise RuntimeError('Failed to insert token') from exc
         return token
