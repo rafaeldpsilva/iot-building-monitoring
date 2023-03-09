a0572f9a067ef07d8bc793837f7daf946baa929d
rafaeldpsilva <1190979@isep.ipp.pt>
2023-03-09 17:02:42 +0000

    get iots

--- core/Core.py
+++ core/Core.py
@@ -16,12 +16,6 @@ class Core(Thread):
         Thread.__init__(self)
         self.iots = []
 
-    def get_forecasted_flexibility(self):
-        forecasted_flexibility = []
-        for iot in self.iots:
-            forecasted_flexibility.append([iot.name, iot.get_power() * random.randrange(0,20)/100])
-        return forecasted_flexibility
-
     def run_thread_schedule(self, job):
         forecast = threading.Thread(target=job)
         forecast.start()
@@ -45,15 +39,14 @@ class Core(Thread):
             monitoring.daemon = True
             monitoring.start()
 
-        #while 1:
-        #    schedule.run_pending()
-        #    time.sleep(1)
-
-        for i in range(len(self.iots)):
-            self.iots[i].join()
+        for i, iot in enumerate(self.iots):
+            iot.join()
 
         storing_manager.join()
         monitoring.join()
+
+    def get_iots(self):
+        return self.iots
 
     def get_total_consumption(self):
         totalPower = 0
@@ -66,3 +59,9 @@ class Core(Thread):
         for iot in self.iots:
             totalCurrent += iot.get_current()
         return totalCurrent
+
+    def get_forecasted_flexibility(self):
+        forecasted_flexibility = []
+        for iot in self.iots:
+            forecasted_flexibility.append([iot.name, iot.get_power() * random.randrange(0,20)/100])
+        return forecasted_flexibility
