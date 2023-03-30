fe74a79e22e3941b21149242085d325525d0dd08
rafaeldpsilva <1190979@isep.ipp.pt>
2023-03-30 12:12:13 +0100

    iot print error

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,11 @@
 import json
 import sys
+from colorama import Fore
 sys.path.append('.')
 
 def get_config():
     with open('./config/config.json') as config_file:
         return json.load(config_file)
 
-def print_error(skk): 
-    print("\033[91m {}\033[00m" .format(skk))
+def print_error(error): 
+    print(Fore.RED + error)
