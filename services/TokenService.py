6bf25ea02539db860343829c2fd2ad8db8e2de4f
rafaeldpsilva <1190979@isep.ipp.pt>
2023-04-13 12:59:34 +0100

    getTokens

--- services/TokenService.py
+++ services/TokenService.py
@@ -1,5 +1,6 @@
 import datetime
 from database.TokenRepository import TokenRepository
+import jwt
 
 class TokenService:
     def __init__(self):
@@ -9,7 +10,8 @@ class TokenService:
         tokens = self.token_repo.get_tokens()
         tokens_array = []
         for token in tokens:
-            tokens_array.append({'token': token['token'], 'expiration_time_minutes': token['expiration_time_minutes'],'datetime': token['datetime']})   
+            decoded = jwt.decode(token, 'thisisthesecretkey', algorithms=["HS256"])
+            tokens_array.append({'name' : decoded['Name'],'List of Resources': decoded['List of Resources'],'token': token['token'], 'expiration_time_minutes': token['expiration_time_minutes'],'datetime': token['datetime']})
         return tokens_array
     
     def insert_token(self,token, exp):
