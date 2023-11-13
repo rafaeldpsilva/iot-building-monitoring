6e78b9ec565fc3d9553fdf57e16f2b5dd539dfdb
rafaeldpsilva <1190979@isep.ipp.pt>
2023-11-13 13:53:53 +0000

    energy prices

--- modules/EnergyPriceAdapter.py
+++ modules/EnergyPriceAdapter.py
@@ -2,16 +2,16 @@ from utils import utils
 
 def get_price_now():
     response = utils.update_values_get("energy price", "192.168.2.68:5000/now")
-    return response
+    return response['price']
 def get_price_next():
     response = utils.update_values_get("energy price", "192.168.2.68:5000/next")
-    return response
+    return response['price']
 def get_price_today():
     response = utils.update_values_get("energy price", "192.168.2.68:5000/today")
-    return response
+    return response['price']
 def get_price_tomorrow():
     response = utils.update_values_get("energy price", "192.168.2.68:5000/tomorrow")
-    return response
+    return response['price']
 def get_price_all():
     response = utils.update_values_get("energy price", "192.168.2.68:5000/all")
-    return response
+    return response['price']
