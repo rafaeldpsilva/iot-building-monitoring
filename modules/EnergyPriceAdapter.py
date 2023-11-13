60fa34d05fee9f09ef167e4f1bf3bae1d14a7d1e
rafaeldpsilva <1190979@isep.ipp.pt>
2023-11-13 12:18:12 +0000

    energy prices

---
+++ modules/EnergyPriceAdapter.py
@@ -0,0 +1,17 @@
+from utils import utils
+
+def get_price_now():
+    response = utils.update_values_get("energy price", "192.168.2.68:5000/now")
+    return response
+def get_price_next():
+    response = utils.update_values_get("energy price", "192.168.2.68:5000/next")
+    return response
+def get_price_today():
+    response = utils.update_values_get("energy price", "192.168.2.68:5000/today")
+    return response
+def get_price_tomorrow():
+    response = utils.update_values_get("energy price", "192.168.2.68:5000/tomorrow")
+    return response
+def get_price_all():
+    response = utils.update_values_get("energy price", "192.168.2.68:5000/all")
+    return response
--- utils/utils.py
+++ utils/utils.py
@@ -14,22 +14,22 @@ def get_config():
     with open('./config/config.json') as config_file:
         return json.load(config_file)
 
-def update_values_get(iot_name, uri):
+def update_values_get(process_name, uri):
     response = None
     try:
         request = requests.get(uri)
         data_json = request.text
         response = json.loads(data_json)
     except requests.exceptions.HTTPError:
-        print_error("HTTPError in " + iot_name)
+        print_error("HTTPError in " + process_name)
     except ConnectionRefusedError:
-        print_error("ConnectionRefusedError in " + iot_name)
+        print_error("ConnectionRefusedError in " + process_name)
     except urllib3.exceptions.NewConnectionError:
-        print_error("NewConnectionError in " + iot_name)
+        print_error("NewConnectionError in " + process_name)
     except urllib3.exceptions.MaxRetryError:
-        print_error("MaxRetryError in " + iot_name)
+        print_error("MaxRetryError in " + process_name)
     except requests.exceptions.ConnectionError:
-        print_error("ConnectionError in " + iot_name)
+        print_error("ConnectionError in " + process_name)
     if response == None:
         return response
     return response
