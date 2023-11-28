5afa9bd4bdb2ff0d9a9a15e455b6b3008b8888ec
rafaeldpsilva <1190979@isep.ipp.pt>
2023-11-28 11:20:41 +0000

    divisions

--- api/main.py
+++ api/main.py
@@ -351,7 +351,6 @@ def production_breakdown():
 def get_divisions():
     division_service = DivisionService()
     divisions = division_service.get_divisions()
-    print(divisions)
     return jsonify({"divisions": divisions})
 
 @app.route('/divisions/create', methods=['POST'])
