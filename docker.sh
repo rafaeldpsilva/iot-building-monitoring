acd0db84913737797948922c74617910eaaa7c94
rafaeldpsilva <rdpds@isep.ipp.pt>
2025-01-07 00:50:15 +0000

    up

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 docker build -t rdpds/building-api .
 docker tag rdpds/building-api rdpds/building-api:latest
-docker tag rdpds/building-api rdpds/building-api:v0.2.11
+docker tag rdpds/building-api rdpds/building-api:v0.2.12
 docker push --all-tags rdpds/building-api
